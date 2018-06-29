31f77731e56cb00de00e6b636dcdcb31dfe49d03
laxbuddy1 <jcyachimowicz131@gmail.com>
2018-06-29 14:28:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get'/reversename/:name'
+    name=paramas[:name]
+    @name.reverse
+end 
 
+
+  get'/say/:number/:phrase'
+  get'/say/:word1/:word2/:word3/:word4/:word5'
+  get'/:operation/:number1/:number2'
 end
+
+/add/1/2
